71a0e8efee357e353a53c94c54f68633fe9e5e93
Sergey_Kataev <nimnul010@yandex.ru>
2023-07-06 14:51:12 +0300

    добавил настройку admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from users.models import CustomUser, Subscribe
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email')
+    list_filter = ('first_name', 'last_name')
+    ordering = ('username', )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Subscribe)
