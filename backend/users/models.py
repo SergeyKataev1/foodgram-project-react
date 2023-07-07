cedfe5c3168d9a9dd9de7c478d01cb04e247a20c
SergayKataev1 <nimnul010@yandex.ru>
2023-07-07 07:48:41 +0300

    дописан user models

--- backend/users/models.py
+++ backend/users/models.py
@@ -20,8 +20,12 @@ class CustomUser(AbstractUser):
     
 
 class Subscribe(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='subscriber')
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='author')
+    user = models.ForeignKey(CustomUser,
+                             on_delete=models.CASCADE,
+                             related_name='subscriber')
+    author = models.ForeignKey(CustomUser,
+                               on_delete=models.CASCADE,
+                               related_name='author')
 
     class Meta:
         verbose_name = 'Подписка'
