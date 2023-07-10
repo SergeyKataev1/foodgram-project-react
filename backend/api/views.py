848c16f0d93625f30b41b33e925ce8ce349f8808
Sergey_Kataev <nimnul010@yandex.ru>
2023-07-10 12:35:41 +0300

    urls views готовы

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,202 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from django.contrib.auth import get_user_model
+from django.db.models import Sum
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
+from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import (ModelViewSet, 
+                                     ReadOnlyModelViewSet)
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django_filters.rest_framework import FilterSet, filters
+from rest_framework.filters import SearchFilter
+from recipes.models import (Favorite, 
+                            Ingredient, 
+                            Recipe, 
+                            RecipeIngredient,
+                            ShoppingCart, 
+                            Tag)
+from users.models import Subscribe
+
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+from api.serializers import (CustomUserCreateSerializer, 
+                          CustomUserSerializer,
+                          FavoriteSerializer, 
+                          IngredientSerializer,
+                          RecipeReadSerializer, 
+                          RecipeSerializer,
+                          ShoppingCartSerializer, 
+                          SubscribeSerializer,
+                          TagSerializer)
+
+User = get_user_model()
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.method in SAFE_METHODS or obj.author == request.user
+
+
+class LimitPageNumberPagination(PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'limit'
+
+
+class AuthorTagFilter(FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(field_name='tags__slug',
+                                             to_field_name='slug',
+                                             queryset=Tag.objects.all())
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(favorite__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(shoppingcart__user=self.request.user)
+        return queryset
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author')
+
+
+class IngredientSearchFilter(SearchFilter):
+    search_param = 'name'
+
+class CustomUserViewSet(UserViewSet):
+    queryset = User.objects.all()
+    serializer_class = CustomUserCreateSerializer
+    pagination_class = LimitPageNumberPagination
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return CustomUserSerializer
+        return super().get_serializer_class()
+
+    @action(detail=True, methods=['post', 'delete'])
+    def subscribe(self, request, id):
+        user = request.user
+        author = get_object_or_404(User, id=id)
+
+        if request.method == 'POST':
+            if user == author:
+                return Response({
+                    'errors': 'Вы не можете подписываться на самого себя'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            if Subscribe.objects.filter(user=user, author=author).exists():
+                return Response({
+                    'errors': 'Вы уже подписаны на данного пользователя'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            follow = Subscribe.objects.create(user=user, author=author)
+            serializer = SubscribeSerializer(follow, context={
+                'request': request
+            })
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        the_subscribe = get_object_or_404(Subscribe, user=user,
+                                          author=author)
+        the_subscribe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False)
+    def subscriptions(self, request):
+        user = request.user
+        queryset = Subscribe.objects.filter(user=user)
+        pages = self.paginate_queryset(queryset)
+        serializer = SubscribeSerializer(pages, context={
+            'request': request,
+        }, many=True)
+        return self.get_paginated_response(serializer.data)
+
+
+class TagViewSet(ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    filter_backends = (IngredientSearchFilter,)
+    search_fields = ('^name',)
+
+
+class RecipeViewSet(ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+    filterset_class = AuthorTagFilter
+    pagination_class = LimitPageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return RecipeReadSerializer
+        return RecipeSerializer
+
+    def add_obj(self, request, serializers, pk):
+        data = {'user': request.user.id, 'recipe': pk}
+        serializer = serializers(data=data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete_obj(self, request, model, id):
+        model_instance = model.objects.filter(user=request.user.id,
+                                              recipe__id=id)
+        if model_instance.exists():
+            model_instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({'error': 'Такого рецепта нет в списке.'
+                         }, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['POST', 'DELETE'])
+    def favorite(self, request, pk):
+        if request.method == 'POST':
+            return self.add_obj(request, FavoriteSerializer, pk)
+
+        return self.delete_obj(request, Favorite, pk)
+
+    @action(detail=True, methods=['POST', 'DELETE'])
+    def shopping_cart(self, request, pk):
+        if request.method == 'POST':
+            return self.add_obj(request, ShoppingCartSerializer, pk)
+
+        return self.delete_obj(request, ShoppingCart, pk)
+
+    def create_shopping_cart(self, ingredients):
+        shopping_list = 'Купить в магазине:'
+        for ingredient in ingredients:
+            shopping_list += (
+                f"\n{ingredient['ingredient__name']} "
+                f"({ingredient['ingredient__measurement_unit']}) - "
+                f"{ingredient['ingredient_value']}")
+        file = 'shopping_list.txt'
+        response = HttpResponse(shopping_list, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename="{file}.txt"'
+        return response
+
+    @action(detail=False, methods=['GET'])
+    def download_shopping_cart(self, request):
+        ingredients = RecipeIngredient.objects.filter(
+            recipe__shoppingcart__user=request.user
+        ).order_by('ingredient__name').values(
+            'ingredient__name', 'ingredient__measurement_unit'
+        ).annotate(ingredient_value=Sum('amount'))
+        if len(ingredients) == 0:
+            return Response({'error': 'Список покупок пуст'
+                             }, status=status.HTTP_400_BAD_REQUEST)
+        return self.create_shopping_cart(ingredients)
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'users',
+    'recipes',
+    'api',
 ]
 
 MIDDLEWARE = [
